e5a722fd2f1d914f08e9ea62ebfe229d0672a715
Benjamin <adolphe906@gmail.com>
2020-11-20 21:07:33 +0100

    TAM ligne + heure + nb Tram

---
+++ src/TAM.java
@@ -0,0 +1,91 @@
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class TAM {
+	private static final String INPUT_PATH = "TAM_MMM_OffreJour";
+	private static final String OUTPUT_PATH = "output/TAM-";
+	private static final Logger LOG = Logger.getLogger(TAM.class.getName());
+
+	static {
+		System.setProperty("java.util.logging.SimpleFormatter.format", "%5$s%n%6$s");
+
+		try {
+			FileHandler fh = new FileHandler("out.log");
+			fh.setFormatter(new SimpleFormatter());
+			LOG.addHandler(fh);
+		} catch (SecurityException | IOException e) {
+			System.exit(1);
+		}
+	}
+
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String[] Values = value.toString().split(";");
+			if (Values[0].equals("course"))
+				return;
+			String NumLigne = Values[4];
+			String[] time = Values[Values.length - 2].split(":");
+			context.write(new Text(NumLigne + "," + time[0]), new IntWritable(1));
+		}
+
+	}
+
+	public static class Reduce extends Reducer<Text, IntWritable, Text, Text> {
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			String[] dataKeys = key.toString().split(",");
+			String ligne = "Ligne " + dataKeys[0];
+			String time = dataKeys[1] + "h";
+			int acc = 0;
+			for (IntWritable i : values) {
+				acc += i.get();
+			}
+			context.write(new Text(ligne), new Text(time + ", " + acc));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("fs.file.impl", "com.conga.services.hadoop.patch.HADOOP_7682.WinLocalFileSystem");
+		Job job = new Job(conf, "Join");
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+
+		job.setOutputValueClass(IntWritable.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(INPUT_PATH));
+		FileOutputFormat.setOutputPath(job, new Path(OUTPUT_PATH + Instant.now().getEpochSecond()));
+
+		job.waitForCompletion(true);
+	}
+
+}
